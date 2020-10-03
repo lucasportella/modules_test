81843e9648c5c92f8be1155c2de7a9d0d698bbf2
lucasportella <lucasportellaagu@gmail.com>
2020-10-03 20:49:10 -0300

    ex107.1 guanabara import 1

---
+++ ex107.1/teste.py
@@ -0,0 +1,6 @@
+import moeda
+preço = float(input('Digite o preço: R$'))
+print(f'A metade de {preço} é {moeda.metade(preço):.2f}')
+print(f'O dobro de {preço} é {moeda.dobro(preço):.2f}')
+print(f'Aumentado em 10%, temos {moeda.aumentar(preço):.2f}')
+print(f'Reduzindo em 13%, temos {moeda.reduzir(preço):.2f}')
